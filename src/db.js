819330ae8dbc38bb490f86fb6bd3f6ce018b3856
santo-sh <santosh.sk11112222@gmail.com>
2022-08-18 23:14:15 +0530

    added modified db.js

--- src/db.js
+++ src/db.js
@@ -3,6 +3,9 @@ const CONFIG = require('./config/index');
 
 const pool = new Pool({
   connectionString: CONFIG.DB.url,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
   port: 5432,
